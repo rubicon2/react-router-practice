ec4de7c0e87375172832d7c26c990ab490e80afc
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-20 10:52:45 +0100

    Create basic header component

---
+++ src/routes/header/index.jsx
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo.png';
+import './index.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="company-info">
+        <img className="company-info--logo" src={logo} alt="" />
+        <div className="company-info--tel">01122 334455</div>
+      </div>
+    </header>
+  );
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router';
+import Header from './header';
 
 export default function Root() {
   return (
     <>
-      <h1>Header</h1>
+      <Header />
       <Outlet />
       <h1>Footer</h1>
     </>
