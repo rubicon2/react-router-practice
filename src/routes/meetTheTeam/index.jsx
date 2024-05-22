aa36dc3e9bcc34f4e33930f7d4603cb2a9919610
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-22 16:41:00 +0100

    Move text-transform uppercase to prevent unwanted side effects

--- src/routes/meetTheTeam/index.jsx
+++ src/routes/meetTheTeam/index.jsx
@@ -5,7 +5,7 @@ import './_index.scss';
 export default function MeetTheTeam() {
   return (
     <div>
-      <h1>Meet The Team</h1>
+      <h1 className="meet-the-team--heading">Meet The Team</h1>
       <p className="half-width">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus a dolore
         aperiam, harum corrupti temporibus. Odio cumque sed harum? Amet!
@@ -13,7 +13,7 @@ export default function MeetTheTeam() {
       {teamData.departments.map((department) => {
         return (
           <div className="meet-the-team--department" key={department.name}>
-            <h2>{department.name}</h2>
+            <h2 className="meet-the-team--heading">{department.name}</h2>
             <div className="meet-the-team--department-members">
               {department.members.map((member) => {
                 return <TeamCard key={member.name} member={member} />;
