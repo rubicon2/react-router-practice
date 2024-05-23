033f859f081c6932a22833d170e2ca4c8761cafb
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-23 11:11:01 +0100

    Create basic home page

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,37 +1,21 @@
+import happyMan from '../../assets/happy-man.png';
+import './_index.scss';
+
 export default function Home() {
   return (
-    <main className="container">
-      <h1 className="page-heading">Home</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        explicabo possimus veniam cumque rem magnam beatae sapiente assumenda!
-        Ratione, odit?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        explicabo possimus veniam cumque rem magnam beatae sapiente assumenda!
-        Ratione, odit?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        explicabo possimus veniam cumque rem magnam beatae sapiente assumenda!
-        Ratione, odit?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        explicabo possimus veniam cumque rem magnam beatae sapiente assumenda!
-        Ratione, odit?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        explicabo possimus veniam cumque rem magnam beatae sapiente assumenda!
-        Ratione, odit?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        explicabo possimus veniam cumque rem magnam beatae sapiente assumenda!
-        Ratione, odit?
-      </p>
+    <main className="container home--container">
+      <h1 className="page-heading home--hero">
+        THE <span className="home--hero-bold">NEXT GENERATION OF WINDOWS</span>{' '}
+        ARE HERE
+      </h1>
+      <div className="grid-two-col">
+        <p className="main-p no-margin">
+          Just when you thought windows were done and dusted! Now we have
+          windows with doors in them, doors with windows in them, now we have
+          windows with doors with windows in them.
+        </p>
+        <img src={happyMan} alt="" />
+      </div>
     </main>
   );
 }
