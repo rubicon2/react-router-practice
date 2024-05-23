521dfc1bec7a051abb456a86b22d4fadd21f0af5
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-23 12:19:55 +0100

    Fill products page with content

--- src/routes/products/index.jsx
+++ src/routes/products/index.jsx
@@ -1,7 +1,52 @@
+import windowImg from '../../assets/product-photos/window.jpg';
+import msWindowsImg from '../../assets/product-photos/microsoft-windows.jpg';
+import './_index.scss';
+
 export default function Products() {
   return (
     <main className="container">
-      <h1 className="page-heading">Products Page</h1>
+      <h1 className="page-heading">Products</h1>
+
+      <div className="products--product-section">
+        <h2 className="products--product-heading">UPVC Windows</h2>
+        <div className="grid-two-col products--product-info">
+          <div>
+            <p className="no-margin">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero
+              ipsum, quisquam aut iusto labore magni nesciunt necessitatibus
+              quae earum ut.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed,
+              vel! Ad dolore totam, laborum expedita iure corrupti numquam
+              dolores. Eius!
+            </p>
+          </div>
+          <img src={windowImg} alt="Some UPVC windows installed in a house" />
+        </div>
+      </div>
+
+      <div className="products--product-section">
+        <h2 className="products--product-heading">Microsoft Windows</h2>
+        <div className="grid-two-col products--product-info">
+          <img
+            src={msWindowsImg}
+            alt="Some UPVC windows installed in a house"
+          />
+          <div>
+            <p className="no-margin">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero
+              ipsum, quisquam aut iusto labore magni nesciunt necessitatibus
+              quae earum ut.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloribus ex ea neque dolores esse dicta consequatur sapiente
+              laudantium illo illum?
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
