f13df269da0fa120e471a27f3c7495b6114c5db8
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-24 16:19:36 +0100

    Convert stickyInfo component css to scss

--- src/routes/stickyInfo/index.jsx
+++ src/routes/stickyInfo/index.jsx
@@ -5,7 +5,7 @@ import './_index.scss';
 export default function StickyInfo() {
   return (
     <div className="sticky-info">
-      <div className="sticky-info-company-details">
+      <div className="sticky-info--company-details">
         <div className="container">
           <img className="sticky-info--company-logo" src={companyLogo} />
           <div className="sticky-info--tel">01122 334455</div>
